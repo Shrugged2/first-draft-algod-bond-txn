82e76ef09b482249aa20ffdad526c254d460dbf5
Rick <42774042+Shrugged2@users.noreply.github.com>
2022-06-12 11:56:19 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+# First of all I dont know what the fuck I am doing
+
+# heres a rough rough draft of what I want the main function here to be
+
+# Begin InitialSwap.pyteal sets off the txn. Maybe a print verify screen?
+
+# champange is popped
+
+# Interest payment is run 3x, every 5 seconds,  for 1% of total txn
+
+
+# Settlement txn checks balance on CityPlace, 
+# checks if they want to re run Inital swap to settle. 
+# then closes with Settlement swap. 
+
+# End
